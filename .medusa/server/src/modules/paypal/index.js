83fdc682aac7175f156ba36532c7e94283e96b91
bthginfo <54976519+bthginfo@users.noreply.github.com>
2026-04-16 10:54:41 +0000

    fix: use module.exports instead of exports.default for paypal provider

--- .medusa/server/src/modules/paypal/index.js
+++ .medusa/server/src/modules/paypal/index.js
@@ -1,10 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const service_1 = __importDefault(require("./service"));
+const service_1 = require("./service");
 const utils_1 = require("@medusajs/framework/utils");
-exports.default = (0, utils_1.ModuleProvider)(utils_1.Modules.PAYMENT, {
-    services: [service_1.default],
+const PayPalService = service_1.default || service_1;
+module.exports = (0, utils_1.ModuleProvider)(utils_1.Modules.PAYMENT, {
+    services: [PayPalService],
 });
